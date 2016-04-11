e16c5def809413bc92f8f4e8a3a8fa224cc35ce3
Harrison Gerber <gerberhe@s.dcsdk12.org>
2016-04-11 10:14:19 -0600

    Completed activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,14 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("4", "Clubs", 4);
+		Card card2 = new Card("Ace", "Hearts", 10);
+		Card card3 = new Card("4", "Clubs", 4);
+		
+		if (card1.matches(card3)) {
+			System.out.println("NICE!");
+		} else {
+			System.out.println("NOT NICE!");
+		}
 	}
 }
