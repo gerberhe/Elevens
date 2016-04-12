461a6832a799120040fbe9ff1aade3422aa853bc
Harrison Gerber <gerberhe@s.dcsdk12.org>
2016-04-12 10:10:19 -0600

    Completed activity 2

--- src/Deck.java
+++ src/Deck.java
@@ -30,7 +30,11 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		cards = new ArrayList<Card>(ranks.length);
+		
+		for (int i = 0; i < ranks.length; i++) {
+			cards.add(new Card(ranks[i], suits[i], values[i]));
+		}
 	}
 
 
@@ -39,7 +43,7 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return cards.size() == 0;
 	}
 
 	/**
@@ -47,7 +51,7 @@ public class Deck {
 	 * @return the number of undealt cards in this deck.
 	 */
 	public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return cards.size();
 	}
 
 	/**
@@ -64,7 +68,10 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		int spot = (int) (Math.random() * cards.size());
+		Card returnCard = cards.get(spot);
+		cards.remove(spot);
+		return returnCard;
 	}
 
 	/**
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -2,12 +2,18 @@
  * This is a class that tests the Deck class.
  */
 public class DeckTester {
+	
+	static String[] ranks = {"Four", "Ace", "Seven", "Jack", "Nine"};
+	static String[] suits = {"Clubs", "Hearts", "Diamonds", "Spades", "Clubs"};
+	static int[] values = {4, 11, 7, 10, 9};
 
 	/**
 	 * The main method in this class checks the Deck operations for consistency.
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		Deck deck1 = new Deck(ranks, suits, values);
+		Card dealt = deck1.deal();
+		System.out.println(dealt.toString());
 	}
 }
